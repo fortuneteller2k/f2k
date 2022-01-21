86794f75b57392556bb0c5cfb570ea69dbf949a3
fortuneteller2k <lythe1107@gmail.com>
2022-01-21 16:30:50 +0800

    dynamically load ListenerAdapters with ClassGraph

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,9 @@
+
 import dev.minn.jda.ktx.light
 import io.github.cdimascio.dotenv.dotenv
-import listeners.About
-import listeners.Help
-import listeners.Latex
+import io.github.classgraph.ClassGraph
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import java.util.*
 
 fun main() {
     val env = dotenv {
@@ -10,8 +11,22 @@ fun main() {
         filename = "env"
     }
 
+    val listeners = LinkedList<ListenerAdapter>().apply {
+        // Scan for ListenerAdapters in the `listener` package
+        val listenerClasses = ClassGraph()
+            .acceptPackages("listeners")
+            .scan()
+            .getSubclasses(ListenerAdapter::class.qualifiedName)
+            .loadClasses(ListenerAdapter::class.java)
+
+        for (l in listenerClasses) {
+            // Construct a new instance, then add it in the list
+            add(l.getDeclaredConstructor().newInstance())
+        }
+    }.toArray() // Then convert the list into an Array, so we can spread it
+
     light(token = env["TOKEN"], intents = emptyList(), enableCoroutines = false) {
-        addEventListeners(About(), Help(), Latex())
+        addEventListeners(*listeners)
         setBulkDeleteSplittingEnabled(false)
         setLargeThreshold(50)
     }.also { it.awaitReady() }
